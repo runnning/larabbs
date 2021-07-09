a77eb551b5910b1751fa8f3aa951ce6e753f07cc
yanWN <2467634970@qq.com>
2021-07-09 23:04:04 +0800

    基础页面结构

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{app()->getLocale()}}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--CSRF Token-->
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>@yield('title','laraBBS')-论坛</title>
+    <!--Styles-->
+    <link href="{{mix('css/app.css')}}" rel="stylesheet">
+</head>
+<body>
+<div id="app" class="{{ route_class() }}-page">
+    @include('layouts._header')
+    <div class="container">
+
+        @include('shared._messages')
+
+        @yield('content')
+
+    </div>
+    @include('layouts._footer')
+</div>
+
+<!-- Scripts -->
+<script src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
