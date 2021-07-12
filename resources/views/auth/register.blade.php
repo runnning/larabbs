b00858889d6528f7e148c9a93ec31de062425439
yanWN <2467634970@qq.com>
2021-07-12 22:56:38 +0800

    注册验证码

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,19 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="captcha" class="col-md-4 col-form-label text-md-right">验证码</label>
+                            <div class="col-md-6">
+                                <input id="captcha" class="form-control @error('captcha') is-invalid @enderror" name="captcha">
+                                <img class="thumbnail captcha mt-3 mb-2" src="{{captcha_src('flat')}}" onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片程序获取验证码">
+                                @error('captcha')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
