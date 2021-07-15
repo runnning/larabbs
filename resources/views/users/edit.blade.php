682aff48d8ee44d0143de1786081b574a7c49f86
yanWN <2467634970@qq.com>
2021-07-15 23:15:27 +0800

    显示个人资料

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,6 +9,7 @@
                        <i class="glyphicon glyphicon-edit"></i>编辑个人资料
                    </h4>
                 </div>
+
                 <div class="card-body">
                     <form action="{{route('users.update',$user->id)}}" method="post" accept-charset="UTF-8">
                         @method('PUT')
@@ -24,9 +25,7 @@
                         </div>
                         <div class="form-group">
                             <label for="introduction">个人简介</label>
-                            <textarea id="introduction" name="introduction" class="form-control" rows="3">
-                            {{old('introduction',$user->introduction)}}
-                        </textarea>
+                            <textarea id="introduction" name="introduction" class="form-control" rows="3">{{old('introduction',$user->introduction)}}</textarea>
                         </div>
                         <div class="well well-sm">
                             <button type="submit" class="btn btn-primary">
