17e4d5a2d2f2f1490df07f83063f774a1d46bc73
yanWN <2467634970@qq.com>
2021-07-16 22:26:23 +0800

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -8,13 +8,14 @@
 namespace App\Handlers;
 
 Use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImageUploadHandler
 {
     //只允许以下后缀名图片上传
     protected $allowed_ext=['png','jpg','gif','jpeg'];
 
-    public function save($file,$floder,$file_prefix){
+    public function save($file,$floder,$file_prefix,$max_width=false){
         $floder_name="uploads/images/$floder/".date("Ym/d",time());
 
         $upload_path=public_path().'/'.$floder_name;
@@ -34,8 +35,30 @@ class ImageUploadHandler
         //将图片移动到目标存储路径中
         $file->move($upload_path,$filename);
 
+        //如果限制了图片宽度,就进行裁剪
+        if($max_width&&$extension!='gif'){
+            $this->reduceSize($upload_path.'/'.$filename,$max_width);
+        }
+
         return [
             'path'=>config('app.url')."/$floder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path,$max_width){
+        //先实例化,传参是文件的磁盘物理路径
+        $image=Image::make($file_path);
+
+        //大小调整
+        $image->resize($max_width,null,function ($constraint){
+            //设置宽度是$max_width,高度等比例缩放
+            $constraint->aspectRatio();
+
+
+            //防止截图时图片尺寸变大
+            $constraint->upsize();
+        });
+        //对图片修改后进行保存
+        $image->save();
     }
 }
