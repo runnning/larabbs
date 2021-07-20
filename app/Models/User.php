9bedaa5b63d65a65eff50ecb530f52ee9e5e39da
yanWN <2467634970@qq.com>
2021-07-20 11:40:19 +0800

    用户可以删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
     public function topics(){
         return $this->hasMany(Topic::class);
     }
+    public function isAuthorOf($model): bool
+    {
+        return $this->id==$model->user_id;
+    }
 }
